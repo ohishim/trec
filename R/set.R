749be9cdfee1ececbce9c4b9c9d0a037aee43e73
ohishim <m.ohishi.stat@gmail.com>
2021-12-09 15:22:49 +0900

    initial commit

--- R/set.R
+++ R/set.R
@@ -6,4 +6,4 @@ BETA <- read.csv("data/coefficients.csv")
 img <- lapply(1:10, function(j){readPNG(paste0("fig", "/", "icon", j, ".png"))})
 
 #example dataset
-exData <- read.csv("data/exData.csv")
+exData <- read.table("data/exData.txt", skip=1)
