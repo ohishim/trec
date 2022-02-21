9917e4fa7fe6265572ef7c4e6e8b8ec4a7af27aa
ohishim <m.ohishi.stat@gmail.com>
2022-02-21 11:27:59 +0900

    Update

--- R/set.R
+++ R/set.R
@@ -3,7 +3,7 @@
 # exData <- read.table("data/exData.txt")
 
 #images of icons (used in 'TREC3')
-ImagesOfIcons <- lapply(1:10, function(j){readPNG(paste0("fig", "/", "icon", j, ".png"))})
+# ImagesOfIcons <- lapply(1:10, function(j){readPNG(paste0("fig", "/", "icon", j, ".png"))})
 
 #estimates for fitting icons (used in 'icon.fit')
-EstForFitIcons <- read.csv("data/coefficients.csv")
+# EstForFitIcons <- read.csv("data/coefficients.csv")
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,11 @@
 ## code to prepare `DATASET` dataset goes here
 
+#---   exported
 exData <- read.table("inst/exData.txt")
 usethis::use_data(exData, overwrite = TRUE)
+
+#---  internal
+EstForFitIcons <- read.csv("inst/coefficients.csv")
+ImagesOfIcons <- lapply(1:10, function(j){readPNG(paste0("inst", "/", "icon", j, ".png"))})
+
+usethis::use_data(EstForFitIcons, ImagesOfIcons, internal=T)
