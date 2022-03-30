6a4337ede0e029b8c5b5a23d799d9517b1efe6e8
ohishim <m.ohishi.stat@gmail.com>
2022-03-30 12:23:46 +0900

    Update

--- R/set.R
+++
@@ -1,9 +0,0 @@
-
-#example dataset
-# exData <- read.table("data/exData.txt")
-
-#images of icons (used in 'TREC3')
-# ImagesOfIcons <- lapply(1:10, function(j){readPNG(paste0("fig", "/", "icon", j, ".png"))})
-
-#estimates for fitting icons (used in 'icon.fit')
-# EstForFitIcons <- read.csv("data/coefficients.csv")
